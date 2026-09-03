d7fb58ef5d2e3d7589c2ec6f861dd32eec8cdd13
paulolacerdasilva <plslacerda@gmail.com>
2026-09-03 17:29:56 -0400

    esqueciSenha

---
+++ app/Views/usuarios/esqueci_senha.php
@@ -0,0 +1,31 @@
+<div class="col-xl-4 col-md-6 mx-auto p-5">
+    <div class="card">
+        <div class="card-header">
+            Esqueceu sua senha??
+        </div>
+        <div class="card-body">
+            <?=Sessao::mensagem('usuario')?>
+            <p class="card-text"><small class="text-muted">Entre com e-mail para recuperar sua senha</small></p>
+
+            <form name="logar" method="POST" action="<?= URL ?>/usuarios/esqueci_senha" class="mt-4">
+            
+                <div class="form-group">
+                    <label for="email">E-mail: <sup class="text-danger">*</sup></label>
+                    <input type='email' name='email' id='email'  class='form-control <?= $dados['email_erro'] ? 'is-invalid' : '' ?>'>
+                    <div class='invalid-feedback'>
+                        <?= $dados['email_erro'] ?>
+                    </div>
+                </div>               
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Logar" class="btn btn-info btn-block">
+                    </div>
+                    <div class="col">
+                        <a href="<?=URL?>/usuarios/cadastrar">Você não tem uma conta? Faça o seu cadastro</a>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
